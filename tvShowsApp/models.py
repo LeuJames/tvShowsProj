7f4732db6df25dac425402dc825d4e06a84de547
James Leu <leujase@gmail.com>
2020-04-22 14:01:11 -0500

    Added back-end form validation to meet Coding Dojo Assignment: Semi-Restful TV Shows Validated requirements.

--- tvShowsApp/models.py
+++ tvShowsApp/models.py
@@ -1,9 +1,48 @@
 from django.db import models
+from datetime import datetime
+
+class ShowManager(models.Manager):
+    def new_show_validator(self, postData):
+        errors = {}
+        if len(postData['title']) > 2:
+            for show in Show.objects.all():
+                if show.title == postData['title']:
+                    errors["uniqueTitle"] = "Show title must be unique"
+        else:
+            errors["title"] = "Show title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show network should be at least 3 characters"
+        if datetime.strptime(postData['release_date'], "%Y-%m-%d" ) > datetime.now():
+            errors['release_date'] = "Release date should be in the past"
+        if postData['release_date'] == '':
+            errors['blankDate'] = "Release date should not be blank" 
+        if len(postData['desc']) > 1:
+            if len(postData['desc']) < 10:
+                errors["desc"] = "Show description is optional but should be at least 10 characters if present"
+        return errors
+
+    def update_show_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Show title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show network should be at least 3 characters"
+        if postData['release_date'] != '':
+            if datetime.strptime(postData['release_date'], "%Y-%m-%d" ) > datetime.now():
+                errors['release_date'] = "Release date should be in the past"
+        else:
+            errors['blankDate'] = "Release date should not be blank"
+
+        if len(postData['desc']) > 1:
+            if len(postData['desc']) < 10:
+                errors["desc"] = "Show description is optional but should be at least 10 characters if present"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
-    release_date = models.DateTimeField()
+    release_date = models.DateField()
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- tvShowsApp/views.py
+++ tvShowsApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Show
+from django.contrib import messages
+from datetime import datetime
+
 
 def index(request):
     return redirect('/shows')
@@ -14,7 +17,15 @@ def shows(request):
     return render(request, 'shows.html', context)
 
 def create(request):
-    if request.method == "POST":
+    errors = Show.objects.new_show_validator(request.POST)
+    if len(errors) > 0:
+        request.session['show'] = request.POST
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        if 'show' in request.session:
+            del request.session['show']
         Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['desc'])
         id = Show.objects.last().id
         print(id)
@@ -22,18 +33,27 @@ def create(request):
 
 def details(request, id):
     context = {
-        'show' : Show.objects.get(id=id)
+        'show' : Show.objects.get(id=id),
     }
     return render(request, 'showDetails.html', context)
 
 def edit(request, id):
     context = {
-        'show' : Show.objects.get(id=id)
+        'show' : Show.objects.get(id=id),
     }
     return render(request, 'editShow.html', context)
 
 def update(request, id):
-    if request.method == "POST":
+    errors = Show.objects.update_show_validator(request.POST)
+    if len(errors) > 0:
+        request.session['showToUpdate'] = request.POST
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{id}/edit')
+    else:
+        print(request.session)
+        if 'showToUpdate' in request.session:
+            del request.session['showToUpdate']
         showToUpdate = Show.objects.get(id=id)
         showToUpdate.title = request.POST['title']
         showToUpdate.network = request.POST['network']
